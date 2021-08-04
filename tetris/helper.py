ca0375edcc7a6ca8d091b15b59fcecbcdf8c6f92
kenhuang <pandadd3356@gmail.com>
2021-08-04 12:40:03 +0800

    fix helper typing

--- tetris/helper.py
+++ tetris/helper.py
@@ -5,21 +5,23 @@ class Position(NamedTuple):
     x: Union[int, float]
     y: Union[int, float]
 
-    def __add__(self, vector: "Vector") -> "Position":
+    def __add__(self, vector: "Vector") -> "Position":  # type: ignore
         return Position(self.x + vector.x, self.y + vector.y)
 
     def __sub__(self, vector: "Vector") -> "Position":
         return Position(self.x - vector.x, self.y - vector.y)
 
-    def __eq__(self, pos: "Position") -> bool:
-        return self.x == pos.x and self.y == pos.y
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Position):
+            return False
+        return self.x == other.x and self.y == other.y
 
 
 class Vector(NamedTuple):
     x: Union[int, float]
     y: Union[int, float]
 
-    def __add__(self, vector: "Vector") -> "Vector":
+    def __add__(self, vector: "Vector") -> "Vector":  # type: ignore
         return Vector(self.x + vector.x, self.y + vector.y)
 
     def __sub__(self, vector: "Vector") -> "Vector":
@@ -31,8 +33,10 @@ class Vector(NamedTuple):
     def __bool__(self) -> bool:
         return False if (not self.x and not self.y) else True
 
-    def __eq__(self, vector: "Vector") -> bool:
-        return self.x == vector.x and self.y == vector.y
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Vector):
+            return False
+        return self.x == other.x and self.y == other.y
 
     def clone(self) -> "Vector":
         return Vector(*self)
